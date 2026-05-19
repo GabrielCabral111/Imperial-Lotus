42c1ae42fd9e466082293ddd395ae69ae599f1d4
GabrielCabral111 <gabrielcabral072@gmail.com>
2026-05-19 00:26:06 -0300

    feat(login): implementa layout completo da tela de login

---
+++ front_end/js/login.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("container_login");
+
+function showRegister() {
+  container.classList.add("active");
+}
+
+function showLogin() {
+  container.classList.remove("active");
+} 
+
